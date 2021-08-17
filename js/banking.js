2c5eda97a33252772700b531e4ecefa3bb72d1db
Tamim <farman.arefin1@outlook.com>
2021-08-17 21:47:57 +0600

    commit

--- js/banking.js
+++ js/banking.js
@@ -2,8 +2,10 @@
 //function for get input and clear input field
 
 function getInputField(getInputId) {
+    debugger;
     const amount = document.getElementById(getInputId);
-    const amountValue = amount.value;
+    const amountValueText = amount.value;
+    const amountValue = parseFloat(amountValueText);
 
     amount.value = '';
     return amountValue;
@@ -20,3 +22,35 @@ function totalAmount(textFieldId, currentAmount) {
 
     previousTotalAmount.innerText = total;
 }
+
+
+//function for update balance 
+
+function updateBalance(depositAmount, isAdd) {
+    const previousBalance = document.getElementById('balance-total');
+    const previousBalanceText = currentBalance.innerText;
+    const balance = parseFloat(previousBalanceText);
+
+
+    if (isAdd) {
+        previousBalance.innerText = balance + depositAmount;
+    }
+    else {
+        previousBalance.innerText = balance - depositAmount;
+    }
+}
+
+
+//Add event listener for deposit amount
+
+document.getElementById('deposit-button').addEventListener('click', function () {
+    const deposit = getInputField('deposit-input');
+    const previousAmount = document.getElementById('deposit-total');
+    const previousAmountText = previousAmount.innerText;
+    const previousDepositAmount = parseFloat(previousAmountText);
+    totalAmount(deposit, previousDepositAmount);
+});
+
+
+//Add event listener for withdraw amount 
+
